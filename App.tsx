a54e8830ad801d3cc4926a8d06f5be1aa3d99f1a
djimenezmsu <djimenez@murraystate.edu>
2024-04-18 11:22:33 -0500

    one signal setup

--- App.tsx
+++ App.tsx
@@ -3,9 +3,26 @@ import { NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './components/HomeScreen';
 import { RootStackParamList } from './data/types';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 function App(): React.JSX.Element {
+  const oneSignalAppId = "4f2a57fd-aca7-459c-b63d-9f90f5498cf6";
   const Stack = createNativeStackNavigator<RootStackParamList>();
+  
+  // Remove this method to stop OneSignal Debugging
+  // OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize(oneSignalAppId);
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  // OneSignal.Notifications.addEventListener('click', (event) => {
+  //   console.log('OneSignal: notification clicked:', event);
+  // });
   
   return (
     <NavigationContainer>
