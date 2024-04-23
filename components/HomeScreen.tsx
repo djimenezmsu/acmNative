aac7f95ec01a22c55e3552410f5e815a9c8c1f88
David Jimenez <djimenez@murraystate.edu>
2024-04-22 21:35:07 -0500

    back button now takes the webview back a page

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,11 +1,35 @@
 import { HomeProps } from "../data/types"
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { BackHandler } from "react-native";
 import WebView from "react-native-webview";
 
 const HomeScreen: React.FC<HomeProps> = (props) => {
+  const webViewRef = useRef<WebView>(null);
+  const handleBackButtonPress = () => {
+    try {
+        webViewRef.current?.goBack();
+        
+        return true;
+    } catch (err: any) {
+        console.log("[handleBackButtonPress] Error : ", err.message);
+
+        return false;
+    }
+  }
+
+  useEffect(() => {
+      BackHandler.addEventListener("hardwareBackPress", handleBackButtonPress)
+      return () => {
+          BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress)
+      };
+  }, []);
 
   return (
-    <WebView source={{ uri: 'https://acm.murraystate.edu/' }} userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36" />
+    <WebView 
+      ref={webViewRef}
+      source={{ uri: 'https://acm.murraystate.edu/' }} 
+      userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+    />
   )
 }
 
